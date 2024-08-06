eae2e1d4e9f6b731fb1719552bb8ab3e565c5180
LakhteHasnain <pakumpire786@gmail.com>
2024-08-06 16:16:54 +0500

    origin correction

--- Server/server.js
+++ Server/server.js
@@ -10,7 +10,7 @@ const notesrouter = require("./router/notes-router");
 const app = express();
 
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: ["http://deploy-mern-1whq.vercel.app"],
     methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
     credentials: true,
 };
