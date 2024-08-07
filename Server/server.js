9c715db09bed0b2890cfe65cd67dcc857161dd77
LakhteHasnain <pakumpire786@gmail.com>
2024-08-07 15:53:00 +0500

    package update

--- Server/server.js
+++ Server/server.js
@@ -10,7 +10,7 @@ const notesrouter = require("./router/notes-router");
 const app = express();
 
 const corsOptions = {
-    origin: ["http://deploy-mern-1whq.vercel.app"],
+    origin: ["https://kitaab-minimal-note-taking-app.netlify.app/"],
     methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
     credentials: true,
 };
